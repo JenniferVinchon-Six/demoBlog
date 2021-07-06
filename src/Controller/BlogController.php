837fb3a6ec84babcb6da29105171e7b133c4a399
Jennifer VINCHON-SIX <sixjennifer@ymail.com>
2021-07-06 09:13:15 +0200

    dev entité + structure HTML CSS + fixtures

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
     /**
+     * Méthode permettant d'afficher l'ensemble des articles du blog
+     * 
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function blog(): Response
     {
-        return $this->render('blog/index.html.twig', [
+        // traitements requete selection BDD des articles
+        // pr exécuter une requete de SELECTION j'ai besoin d'appeler la class Article du fichier ArticleRepository ds le dossier Repository
+        // $repoArticles : est un objet issu de la class ArticleRepository
+        $repoArticles = $this->getDoctrine()->getRepository(Article::class);
+        dump($repoArticles);// dump(): outil de debug, on affiche se que contient $repoArticles
+
+        $articles = $repoArticles->findAll();
+        dump($articles);
+
+        return $this->render('blog/blog.html.twig', [
             'controller_name' => 'BlogController',
         ]);
     }
@@ -29,5 +41,15 @@ class BlogController extends AbstractController
             "title" => "Blog dédié à la musique, venez voir mais surtout écouter !!!",
             "age" => 25
         ]);
+    }
+
+    /**
+     * Méthode permettant d'afficher le détail/ le contenu d'un article
+     * 
+     * @Route("/blog/12", name="blog_show")
+     */
+    public function show(): Response
+    {
+        return $this->render('blog/show.html.twig');
     }
 }
